cf8e42c047b6b28f58546bc1a63d2693897ee94d
Harshad4387 <harshadjogdande25@gmail.com>
2025-06-26 23:10:49 +0530

    subscription schema added

---
+++ startwithproject/src/models/subscription.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const subscriptionschema = mongoose.Schema({
+    subscriber : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'user'
+    },
+    channel : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'user'
+    }
+},{timestamps: true});
+
+const subscription = mongoose.model("subscription",subscriptionschema);
+module.exports = subscription;
